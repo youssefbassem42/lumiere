e8b34812f3d43e7e6616c023f30a18828f8da758
youssefbassem42 <youssefbassem42@gmail.com>
2026-08-19 12:29:25 +0300

    fix: update widget script data-key for consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,8 +35,9 @@ export default function RootLayout({
           <Footer />
         </Providers>
         <Script  
-          src="https://aicommerce-ai-service-production.up.railway.app/widget/v1/widget.js"
-          data-widget-key="wi_ek-n5e_bDglu9pHAFDlag5xb6OUxESkDwDBoYIS_wgA"
+            src="https://aicommerce-ai-service-production.up.railway.app/widget/v1/widget.js"
+            data-widget-key="wi_8564d4c9-c2e0-4cbc-b43c-ae891c19ebc0"
+
 />
       </body>
     </html>
