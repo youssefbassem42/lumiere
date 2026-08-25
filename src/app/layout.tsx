ab0109027dc078d97a34afa4aa82b50a4f89b14d
ArwaMostafa19 <149114522+ArwaMostafa19@users.noreply.github.com>
2026-08-25 15:55:17 +0300

    Remove AI commerce widget scritpt from layout
    Removed external script for AI commerce widget from layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,12 +34,7 @@ export default function RootLayout({
           <main className="flex-1 pt-20">{children}</main>
           <Footer />
         </Providers>
-        <Script  
-          
-  src="https://aicommerce-ai-service-production.up.railway.app/widget/v1/widget.js"
-  data-widget-key="wi_OxZkUu_w-IygwpK6WI9fcJMoGxkIm8RDbP4o49AbyAc"
 
-/>
       </body>
     </html>
   );
